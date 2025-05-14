5162e01347c9551a8b2e30ceb88f2a999d832339
Wandson | Notebook <wandson7emanuel@hotmail.com>
2025-05-13 21:11:33 -0300

    Hotfix; interrupcao

--- Prática 3/interrupcao/interrupcao.ino
+++ Prática 3/interrupcao/interrupcao.ino
@@ -1,43 +1,45 @@
-#define F_CPU 16000000UL
-#include <avr/io.h>
-#include <util/delay.h>
+  #define F_CPU 16000000UL
+  #include <avr/io.h>
+  #include <util/delay.h>
 
-int i = 0;
+  int i = 0;
 
-int main (void){
+  int main (void){
 
-  EICRA = (1<<ISC11) | (1<<ISC10) | (1<<ISC01) | (1<<ISC00);
-  EIMSK = (1<<INT1) | (1<<INT0);
+    DDRB = 0b00111111;
 
-  sei();
+    EICRA = (1<<ISC11) | (1<<ISC10) | (1<<ISC01) | (1<<ISC00);
+    EIMSK = (1<<INT1) | (1<<INT0);
+
+    sei();
 
 
-  while(1){
+    while(1){
+
+    }
 
   }
 
-}
+  // Incrementa
 
-// Incrementa
-
-ISR(INT0_vect){
-  if(i > 6){
-    i = 6;
-    return;
-  }  
-  PORTB ^= (1<<i);
-  i++;
-}
-
-// Decrementa
-
-ISR(INT1_vect){  
-  if(i <= 0){
-    i = 0;
-    return;
+  ISR(INT0_vect){
+    if(i > 6){
+      i = 6;
+      return;
+    }  
+    PORTB ^= (1<<i);
+    i++;
   }
 
-  i--;
-  PORTB ^= (1<<i);
+  // Decrementa
 
-}
+  ISR(INT1_vect){  
+    if(i <= 0){
+      i = 0;
+      return;
+    }
+
+    i--;
+    PORTB ^= (1<<i);
+
+  }
